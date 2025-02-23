4de83192add28244122633fcaa97a4e4cfe8d24b
pab-h <dev.pab.2020@gmail.com>
2025-02-22 22:21:34 -0300

    feat: personagem é uma caixa feat: é possível mover o personagem

--- src/entity/Player.js
+++ src/entity/Player.js
@@ -3,21 +3,33 @@ import * as CANNON from "cannon-es";
 
 class Player {
 
-    constructor(radius) {
-        this.radius   = radius;
+    constructor({ width, height, depth }) {
+        this.width  = width;
+        this.height = height;
+        this.depth  = depth;
         
 
-        this.geometry = new THREE.SphereGeometry(this.radius);
+        this.geometry = new THREE.BoxGeometry(
+            this.width,
+            this.height,
+            this.depth
+        );
         this.material = new THREE.MeshNormalMaterial();
         this.mesh     = new THREE.Mesh(
             this.geometry,
             this.material
         );
 
-
+        // vejam https://discourse.threejs.org/t/three-js-and-cannon-js-are-different-in-width-property-of-object/47813
         this.body = new CANNON.Body({
-            mass: 5,
-            shape: new CANNON.Sphere(this.radius)
+            mass: 10,
+            shape: new CANNON.Box(
+                new THREE.Vector3(
+                    this.width / 2, 
+                    this.height / 2, 
+                    this.depth / 2
+                )
+            )
         });
 
         this.body.position.set(0, 10, 0);
@@ -27,6 +39,28 @@ class Player {
     update() {
         this.mesh.position.copy(this.body.position);
         this.mesh.quaternion.copy(this.body.quaternion);
+    }
+
+    keypress(e) {
+
+        const { key } = e;
+
+        if (key == "w") {
+            this.body.position.z -= 1
+        }
+
+        if (key == "s") {
+            this.body.position.z += 1
+        }
+
+        if (key == "a") {
+            this.body.position.x -= 1
+        }
+
+        if (key == "d") {
+            this.body.position.x += 1
+        }
+
     }
 
 };
